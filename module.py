693ffa386fe23df5820cae7922f9fd62d856f1e7
AndyDawkins <andy.dawkins@glassesdirect.com>
2013-03-07 21:44:29 +0000

    More evil tabs

--- module.py
+++ module.py
@@ -24,110 +24,110 @@ from util import XmlRetrieval
 
 
 class Module:
-	def __init__(self, skills, mod_name):
-		self.stats = {}
-		self.name = mod_name
-		self.skills = skills
+    def __init__(self, skills, mod_name):
+        self.stats = {}
+        self.name = mod_name
+        self.skills = skills
 
-		module_data = XmlRetrieval('module.xml')
-		properties, effecting_skills = module_data.get_target(mod_name)
-		self._add_stats(properties,effecting_skills)
+        module_data = XmlRetrieval('module.xml')
+        properties, effecting_skills = module_data.get_target(mod_name)
+        self._add_stats(properties,effecting_skills)
 
-	def show_stats(self):
-		print self.name
-		for key in self.stats:
-			print '{:<20} {:<15}'.format(key, self.stats[key])
+    def show_stats(self):
+        print self.name
+        for key in self.stats:
+            print '{:<20} {:<15}'.format(key, self.stats[key])
 
-	def get(self, stat):
-		if stat in self.stats:
-			return self.stats[stat]
-		else:
-			return None
+    def get(self, stat):
+        if stat in self.stats:
+            return self.stats[stat]
+        else:
+            return None
 
-	def _add_stats(self, properties, effecting_skills):
-		""" Uses the properties list and effecting_skills list to populate
-		the stats dictionary with all appropriate values. 
-		THIS ALSO HANDLES SKILL BONUSES! """
+    def _add_stats(self, properties, effecting_skills):
+        """ Uses the properties list and effecting_skills list to populate
+        the stats dictionary with all appropriate values.
+        THIS ALSO HANDLES SKILL BONUSES! """
 
-		def _get_skill_modifier(skill_list):
-			output = 0
-			for name in skill_list:
-				try:
-					output = output + self.skills[name]
-				except KeyError:
-					# Skill is not in self.skills
-					pass
-			return output
+        def _get_skill_modifier(skill_list):
+            output = 0
+            for name in skill_list:
+                try:
+                    output = output + self.skills[name]
+                except KeyError:
+                    # Skill is not in self.skills
+                    pass
+            return output
 
-		for key in properties:
-			if key in effecting_skills.keys():
-				# Skills effect this property. Get and apply the skill modifier.
-				skill_list = effecting_skills[key]
-				mod = _get_skill_modifier(skill_list)
-				self.stats[key] = math.floor(properties[key] * (1 + mod))
-			else:
-				self.stats[key] = properties[key]
+        for key in properties:
+            if key in effecting_skills.keys():
+                # Skills effect this property. Get and apply the skill modifier.
+                skill_list = effecting_skills[key]
+                mod = _get_skill_modifier(skill_list)
+                self.stats[key] = math.floor(properties[key] * (1 + mod))
+            else:
+                self.stats[key] = properties[key]
 
 
 class Weapon(Module):
-	""" Req dropsuit and fitting to test properly. """
-	def __init__(self, skills, weapon_name, module_list=[]):
-		self.stats = {}
-		self.name = weapon_name
-		self.skills = skills
-		self.module_list = module_list
+    """ Req dropsuit and fitting to test properly. """
+    def __init__(self, skills, weapon_name, module_list=[]):
+        self.stats = {}
+        self.name = weapon_name
+        self.skills = skills
+        self.module_list = module_list
 
-		weapon_data = XmlRetrieval('weapon.xml')
-		properties, effecting_skills = weapon_data.get_target(weapon_name)
-		self._add_stats(properties,effecting_skills)
-		self._add_module_bonus()
+        weapon_data = XmlRetrieval('weapon.xml')
+        properties, effecting_skills = weapon_data.get_target(weapon_name)
+        self._add_stats(properties,effecting_skills)
+        self._add_module_bonus()
 
-	def _add_module_bonus(self):
-		""" Searching self.module_list for any modules which effect this 
-		weapons slot type (found in the modules 'enhances' cell. If so, it will 
-		add the bonuses for that module. 
-		CURRENTLY ONLY WORKS FOR DAMAGE!!! """
-		slot_type = self.stats['slot_type']
-		for m in self.module_list:
-			try:
-				if slot_type == m.stats['enhances']:
-					self.stats['damage'] = self.stats['damage'] * (1 + m.stats['damage'])
-			except KeyError:
-				# Module does not have a key 'enhances' in its stats dictionary.
-				pass
+    def _add_module_bonus(self):
+        """ Searching self.module_list for any modules which effect this
+        weapons slot type (found in the modules 'enhances' cell. If so, it will
+        add the bonuses for that module.
+        CURRENTLY ONLY WORKS FOR DAMAGE!!! """
+        slot_type = self.stats['slot_type']
+        for m in self.module_list:
+            try:
+                if slot_type == m.stats['enhances']:
+                    self.stats['damage'] = self.stats['damage'] * (1 + m.stats['damage'])
+            except KeyError:
+                # Module does not have a key 'enhances' in its stats dictionary.
+                pass
 
 
 class ModuleLibrary:
-	def __init__(self):
-		self.module_data = XmlRetrieval('module.xml')
+    def __init__(self):
+        self.module_data = XmlRetrieval('module.xml')
 
-		self.names = self.module_data.get_list()
+        self.names = self.module_data.get_list()
 
-	def get_names(self):
-		""" Returns module names as a tuple. """
-		return tuple(self.names)
+    def get_names(self):
+        """ Returns module names as a tuple. """
+        return tuple(self.names)
 
-	def get_parents(self):
-		return self.module_data.get_parents()
+    def get_parents(self):
+        return self.module_data.get_parents()
 
-	def get_children(self, parent):
-		return self.module_data.get_children(parent)
-			
+    def get_children(self, parent):
+        return self.module_data.get_children(parent)
+
 
 class WeaponLibrary(ModuleLibrary):
-	def __init__(self):
-		self.weapon_data = XmlRetrieval('weapon.xml')
+    def __init__(self):
+        self.weapon_data = XmlRetrieval('weapon.xml')
 
-		self.names = self.weapon_data.get_list()
+        self.names = self.weapon_data.get_list()
 
-	def get_parents(self):
-		return self.weapon_data.get_parents()
+    def get_parents(self):
+        return self.weapon_data.get_parents()
 
-	def get_children(self, parent):
-		return self.weapon_data.get_children(parent)
+    def get_children(self, parent):
+        return self.weapon_data.get_children(parent)
 
 
 if __name__ == '__main__':
-	modlib = ModuleLibrary()
-	print modlib.get_parents()
-	print modlib.get_children('shields')
+    modlib = ModuleLibrary()
+    print modlib.get_parents()
+    print modlib.get_children('shields')
